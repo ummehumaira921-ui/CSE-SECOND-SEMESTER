9610a038c8b1991e1c27deb1a8eb589fa3c2997b
ummehumaira921-ui <ummehumaira921@gmail.com>
2026-02-15 22:10:50 +0600

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+
+        input.close();
+    }
+}
