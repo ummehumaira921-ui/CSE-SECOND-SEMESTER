292b3bc8253334942eb25fbc6afb74925e7123aa
ummehumaira921-ui <ummehumaira921@gmail.com>
2026-02-15 21:54:30 +0600

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
