f14ed444820c47593772f91c3471732698119bca
ummehumaira921-ui <ummehumaira921@gmail.com>
2026-02-15 22:03:29 +0600

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest;
+
+        if (a >= b && a >= c) {
+            largest = a;
+        } else if (b >= a && b >= c) {
+            largest = b;
+        } else {
+            largest = c;
+        }
+
+        System.out.println("Largest number = " + largest);
+
+        sc.close();
+    }
+}
